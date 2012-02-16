23ed3040750508b4fc3519d46d3643771d589d95
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2012-02-16 00:06:56 -0800

    LRUs for username and hashtags.

---
+++ www/js/UsersAndTags.js
@@ -0,0 +1,58 @@
+var UsersAndTags = Class(
+{
+  constructor: function(account)
+  {
+    this._account = account;
+    this._usersLRU = new xo.LRU(1000);
+    this._tagsLRU = new xo.LRU(100);
+  },
+
+  addUser: function(screenname, name)
+  {
+    var val =
+    {
+      key: screenname.toLowerCase(),
+      name: name,
+      screenname: screenname
+    };
+    this._usersLRU.add(name.toLowerCase(), val);
+    this._usersLRU.add(val.key, val);
+  },
+
+  suggestUser: function(partialName)
+  {
+    var seen = {};
+    var matches = [];
+    this._usersLRU.keys().forEach(function(key)
+    {
+      if (key.indexOf(partialName) === 0)
+      {
+        var val = this._usersLRU.get(key);
+        if (!seen[val.key])
+        {
+          seen[val.key] = true;
+          matches.push(val);
+        }
+      }
+    }, this);
+    return matches;
+  },
+
+  addHashtag: function(tag)
+  {
+    this._tagsLRU.add(tag, { name: tag });
+  },
+
+  suggestHashtag: function(partialTag)
+  {
+    var matches = [];
+    this._tagsLRU.keys().forEach(function(key)
+    {
+      if (key.indexOf(partialTag) === 0)
+      {
+        matches.push(this._tagsLRU.get(key));
+      }
+    }, this);
+    return matches;
+  }
+});
