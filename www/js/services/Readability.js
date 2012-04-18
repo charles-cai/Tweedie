72c2115b0d0c0742c80c39232b70a10ded4aa278
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2012-04-18 14:39:02 -0700

    Make more of an attempt to get Readability data.

--- www/js/services/Readability.js
+++ www/js/services/Readability.js
@@ -3,14 +3,14 @@
   var ReadabilityModel = Model.create(
   {
     title: Model.Property,
-    text: Model.Property
+    text: Model.Property,
+    error: Model.Property
   });
 
   var lgrid = grid.get({ lru: 5 });
   var selector = /^\/readable=(.*)$/;
   var pending = null;
   var stage = document.createElement("div");
-
 
   lgrid.watch(selector, function(op, path)
   {
@@ -33,33 +33,89 @@
           {
             method: "POST",
             url: "http://www.readability.com/articles/queue",
-            data: "url=" + url,
+            parameters:
+            {
+              url: url
+            },
             _gridPath: path
           };
           return Ajax.create(pending);
         },
         function(r)
         {
+          var text;
           try
           {
             pending = null;
-            stage.innerHTML = r().text();
+            text = r().text();
+            // Remove any iframes (primitive)
+            text = text.replace(/<iframe.*?>.*?<\/iframe>/ig, "");
+            stage.innerHTML = text;
             model.delayUpdate(function()
             {
               this.title(stage.querySelector("#article-entry-title,#rdb-article-title").innerHTML);
               this.text(stage.querySelector("#rdb-article-content").innerHTML);
             });
+            return;
           }
           catch (e)
           {
-            Log.exception("Readability failure", e);
-            model.delayUpdate(function()
-            {
-              this.title("Failed");
-              this.text(url);
-            });
-            lgrid.remove(path);
+            // Failed - maybe the Readability selection page - look for the id inside it
           }
+
+          var s = text.indexOf("articleId");
+          if (s === -1)
+          {
+            throw e;
+          }
+
+          var articleId = eval(text.slice(s, text.indexOf(",", s)));
+          if (!articleId)
+          {
+            throw Error();
+          }
+          return Co.Routine(this,
+            function()
+            {
+              // Refetch the page explicityly.  We set the readbar which forces Readability to
+              // return the condensed version of the page.
+              pending =
+              {
+                method: "GET",
+                url: "http://www.readability.com/articles/" + articleId,
+                parameters:
+                {
+                  readbar: 1
+                },
+                _gridPath: path
+              };
+              return Ajax.create(pending);
+            },
+            function(r)
+            {
+              pending = null;
+              text = r().text();
+              // Remove any iframes (primitive)
+              text = text.replace(/<iframe.*?>.*?<\/iframe>/ig, "");
+              stage.innerHTML = text;
+              model.delayUpdate(function()
+              {
+                this.title(stage.querySelector("#article-entry-title,#rdb-article-title").innerHTML);
+                this.text(stage.querySelector("#rdb-article-content").innerHTML);
+              });
+            }
+          );
+        },
+        function(_)
+        {   
+          Log.exception("Readability failure");
+          model.delayUpdate(function()
+          {
+            this.title("Failed");
+            this.text(url);
+            this.error(true);
+          });
+          lgrid.remove(path);
         }
       );
     }
